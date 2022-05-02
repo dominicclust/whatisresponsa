1d6a33c4d085e3e99cfad1c3c684020951c85a56
dominicclust <dclust34@gmail.com>
2022-05-02 11:40:03 -0500

    scripts done.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     router.get('/', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
 
     router.get(/^(?!\/?api).*/, (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
@@ -26,14 +26,8 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+        res.status(201).json({});
     })
 }
-
-
-router.get('/hello/world', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!')
-})
 
 module.exports = router;
